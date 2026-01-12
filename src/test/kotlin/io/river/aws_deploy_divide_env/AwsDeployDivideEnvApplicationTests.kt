743d39b96644a11f9138aa2f966eddecd2f362c8
GyeongHun <riverpower6@naver.com>
2026-01-12 23:14:58 +0900

    [FEAT] 실습을 위한 프로퍼티 등 설정
    1. Dockerfile 작성
      - 빌드&서버 멀티 스테이지로 구성
      - 각 스테이지는 각자의 역할만 수행
    2. 환경 프로필 분리
    3. 각 프로필별 프로퍼티 설정

--- src/test/kotlin/io/river/aws_deploy_divide_env/AwsDeployDivideEnvApplicationTests.kt
+++ src/test/kotlin/io/river/aws_deploy_divide_env/AwsDeployDivideEnvApplicationTests.kt
@@ -2,8 +2,10 @@ package io.river.aws_deploy_divide_env
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AwsDeployDivideEnvApplicationTests {
 
     @Test
