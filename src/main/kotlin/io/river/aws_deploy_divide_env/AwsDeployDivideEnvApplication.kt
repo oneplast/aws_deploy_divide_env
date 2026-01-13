ede7276b59dd6e0e3acbd82447bfbf3af8b12307
Gyeonghun Min <riverpower6@naver.com>
2026-01-13 16:08:34 +0900

    [FEAT] 레디스 캐시 사용 및 서버 부팅 시 테스트

--- src/main/kotlin/io/river/aws_deploy_divide_env/AwsDeployDivideEnvApplication.kt
+++ src/main/kotlin/io/river/aws_deploy_divide_env/AwsDeployDivideEnvApplication.kt
@@ -2,8 +2,10 @@ package io.river.aws_deploy_divide_env
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 class AwsDeployDivideEnvApplication
--- src/main/kotlin/io/river/aws_deploy_divide_env/global/initData/BaseInitData.kt
+++ src/main/kotlin/io/river/aws_deploy_divide_env/global/initData/BaseInitData.kt
@@ -3,6 +3,7 @@ package io.river.aws_deploy_divide_env.global.initData
 import io.river.aws_deploy_divide_env.domain.member.member.service.MemberService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationRunner
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Lazy
@@ -27,6 +28,16 @@ class BaseInitData(
     fun work1() {
         if (memberService.count() == 0L) {
             memberService.save("admin", "1234", "관리자")
+
+            self.get1Plus1()
+            self.get1Plus1()
+            self.get1Plus1()
         }
+    }
+
+    @Cacheable("get1Plus1")
+    fun get1Plus1(): Int {
+        println("get1Plus1 run!")
+        return 1 + 1
     }
 }
