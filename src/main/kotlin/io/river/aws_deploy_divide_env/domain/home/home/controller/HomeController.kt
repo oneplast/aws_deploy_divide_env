b39d3ce4785730f0750bc0fdb6eb5cbeddbd8fd2
GyeongHun <riverpower6@naver.com>
2026-01-14 12:08:34 +0900

    [FEAT] 홈 컨트롤러 추가 - 회원 수 측정

---
+++ src/main/kotlin/io/river/aws_deploy_divide_env/domain/home/home/controller/HomeController.kt
@@ -0,0 +1,14 @@
+package io.river.aws_deploy_divide_env.domain.home.home.controller
+
+import io.river.aws_deploy_divide_env.domain.member.member.service.MemberService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController(
+    private val memberService: MemberService
+) {
+
+    @GetMapping("/")
+    fun main() = "회원 수 : ${memberService.count()}"
+}
